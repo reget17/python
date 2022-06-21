4da340ea0a91095629ce0412845bc40266e8edb3
reget17 <reget17@gmail.com>
2022-06-21 14:55:04 +0300

    Password Check

---
+++ scripting/pswd_check/pswd_check.py
@@ -0,0 +1,28 @@
+import requests
+import hashlib
+import sys
+
+
+def get_response(args):
+    for pswd in args:
+        head_pswd, tail_pswd = sha1_encode(pswd)
+        url = 'https://api.pwnedpasswords.com/range/' + head_pswd
+        response = requests.get(url)
+        check_password(response, tail_pswd, pswd)
+        
+
+def sha1_encode(password):
+    hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    # print(hash)
+    head = hash[:5]
+    tail = hash[5:]
+    return head, tail
+
+def check_password(response, hash, password):
+    hashes = (line.split(':') for line in response.text.splitlines())
+    for pswd, count in hashes:
+        if pswd == hash:
+            return print(f'You should change your password. Password "{password}": {count} times')
+    return print(f'Your password {password} is best')   
+
+get_response(sys.argv[1:])
