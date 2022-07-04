3f6a84f1ee99284248fdd9fa6eaa8a2a5d45a088
reget17 <reget17@gmail.com>
2022-07-04 18:38:13 +0300

    edit files update

--- files/1_rename files/rename.py
+++ files/1_rename files/rename.py
@@ -5,11 +5,16 @@ def main():
     lines = []
     with open("SQL and PostgreSQL The Complete Developer's Guide.txt") as file:
         lines = file.readlines()
-        lines = [re.sub(r"[<>:/\|?*]", '', line.rstrip()) for line in lines]
+        lines = [re.sub(r"[<>:/\|\"?*]", '', line.rstrip()) for line in lines]
 
-    end_count = 4
+    end_count = 280
 
     for i in range(end_count):
-        os.replace(f"lesson{i+1}.mp4", f"{i+1}_{lines[i]}.mp4")
+        try:
+            os.replace(f"lesson{i+1}.mp4", f"{i+1}_{lines[i]}.mp4")
+            os.replace(f"lesson{i+1}.srt", f"{i+1}_{lines[i]}.srt")
+        except Exception:
+            print(f"lesson{i+1} не существует")
+            continue
 
 main()
